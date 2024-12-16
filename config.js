ce923d78e338eb9bde58567ca993680dbbc0c8a4
shenmx77 <shenmd11@outlook.com>
2024-12-16 18:24:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: 'put session',
-GITHUB_AUTH_TOKEN: 'put token', //"ghp_ " මෙම කොටස ඉවත් කර token එක දාන්න.
-GITHUB_USER_NAME: 'put your github user name',
+SESSION_ID: 'DARK-SHUTER-MD=LaJm1JSa#_fML4td-L2N4ZdiWeGB91QN-6h-Tv-wUXoN7wnNIamE',
+GITHUB_AUTH_TOKEN: 'r7W3umeDm5RyH4GJsk1cUwVErEcwMw2P9mrt', //"ghp_ " මෙම කොටස ඉවත් කර token එක දාන්න.
+GITHUB_USER_NAME: 'shenmx77',
 
 };
